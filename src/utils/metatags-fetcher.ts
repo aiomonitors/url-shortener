4d23d26939db7c2f7a795da31006e5533f31f0f3
shihab <sachow168@gmail.com>
2021-05-04 23:00:29 -0400

    utils

---
+++ src/utils/metatags-fetcher.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { MetatagsResponse } from "../types";
+
+const fetcher = async (url: string): Promise<MetatagsResponse> => {
+  const response = await axios.get<MetatagsResponse>(
+    `https://www.shihab.dev/api/metadata?url=${url}`
+  );
+  return response.data;
+};
+
+export default fetcher;
