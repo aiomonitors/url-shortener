476a484a885c39844bdf8597df1c7e72a5eab181
shihab <sachow168@gmail.com>
2021-05-04 23:00:05 -0400

    types

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export interface MetatagsResponse {
+  "csrf-param": string;
+  "csrf-token": string;
+  "appleid-signin-client-id": string;
+  "appleid-signin-scope": string;
+  "appleid-signin-redirect-uri": string;
+  "appleid-signin-state": string;
+  "appleid-signin-use-popup": string;
+  viewport: string;
+  "twitter:card": string;
+  "twitter:site": string;
+  "msapplication-tilecolor": string;
+  "msapplication-tileimage": string;
+  "theme-color": string;
+  "og:image": string;
+  "og:url": string;
+  "og:title": string;
+  "og:description": string;
+  charset: null;
+  title: string;
+  url: string;
+  favicon: string;
+  size: number[];
+}
